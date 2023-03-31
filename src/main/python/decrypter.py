7e807dc68871ad309dc0e34dc531462c4e93686e
3rickDJ <erickdejesusmolina@gmail.com>
2023-03-31 00:12:59 -0600

    style: format code

--- src/main/python/decrypter.py
+++ src/main/python/decrypter.py
@@ -1,38 +1,77 @@
-import time 
+import time
+
+
 def wordBreak(dictionary, str, lookup, result):
     n = len(str)
 
-    if n ==0:
+    if n == 0:
         return True
 
     if lookup[n] == -1:
         lookup[n] = 0
-        for i in range(1, n +1):
+        for i in range(1, n + 1):
             prefix = str[:i]
-            if (prefix in dictionary and wordBreak(dictionary, str[i:], lookup, result)):
+            if (prefix in dictionary and wordBreak(
+                    dictionary, str[i:], lookup, result)):
                 lookup[n] = 1
                 result.insert(0, prefix)
                 return True
 
     return lookup[n] == 1
 
+
 if __name__ == '__main__':
     start = time.time()
-    dictionary = [ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "me", "you", "we", "to", "with", "please", "help", "write", "read", "think", "apples", "bananas", "melons", "mangoes", "mushrooms", "strawberries", "can", "give", "and", "not", "do", "want", "build", "together", "houses"] 
+    dictionary = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "me",
+        "you",
+        "we",
+        "to",
+        "with",
+        "please",
+        "help",
+        "write",
+        "read",
+        "think",
+        "apples",
+        "bananas",
+        "melons",
+        "mangoes",
+        "mushrooms",
+        "strawberries",
+        "can",
+        "give",
+        "and",
+        "not",
+        "do",
+        "want",
+        "build",
+        "together",
+        "houses"]
 
-    # sample scytale ciphertext 
+    # sample scytale ciphertext
     input = "dbtheouoevyigleolepnudtmmwhheaaoegnnurigtsavoteneeosdss"
-    
+
     now = 0
-    for i in range(1, len(input) -1):
-        str= ""
+    for i in range(1, len(input) - 1):
+        str = ""
         str += (input[0])
-        for j in range(len(input) -1):
+        for j in range(len(input) - 1):
             now += i
-            if (now > len(input) -1):
-                now = now % (len(input) -1)
-            str+= input[now]
-        str = str.replace('_','')
+            if (now > len(input) - 1):
+                now = now % (len(input) - 1)
+            str += input[now]
+        str = str.replace('_', '')
         lookup = [-1] * (len(str) + 1)
         result = []
         if wordBreak(dictionary, str, lookup, result):
@@ -42,7 +81,7 @@ if __name__ == '__main__':
             print()
             break
         else:
-            if(i ==len(input) -2):
+            if (i == len(input) - 2):
                 print("Scytale doesn't have any secret message")
     end = time.time()
-    print(round(end-start, 8), "seconds are needed to find the solution above")
+    print(round(end - start, 8), "seconds are needed to find the solution above")
