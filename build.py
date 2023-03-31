809cfe879065c9768f956348542baa50bf669fc0
3rickDJ <erickdejesusmolina@gmail.com>
2023-03-30 23:36:59 -0600

    build: add flake8 and coverage

--- build.py
+++ build.py
@@ -3,6 +3,8 @@ from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
 use_plugin("python.unittest")
+use_plugin("python.flake8")
+use_plugin("python.distutils")
 
 
 name = "scytale"
