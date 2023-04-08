3a94c80e7a5dc53d2ee12db8c6f9963dc1290e5d
3rick <erick.dejesus1@alumno.buap.mx>
2023-04-07 22:19:59 -0600

    feat: implt encrypt

--- lib/scytale.rb
+++ lib/scytale.rb
@@ -2,22 +2,32 @@
 
 # Scytale Cipher
 class Scytale
-  def self.encrypt(msg, key, _long)
-    matrix = [[]] * key
-    msg.chars.each_with_index do |char, index|
-      matrix[index % key] << char
-    end
-    matrix.join
+  def encrypt(msg, key)
+    l = (msg.size / key.to_f).ceil
+    msg.ljust(l * key, '*').chars.each_slice(l).to_a.transpose.join
   end
 
   def decrypt(msg, key)
     "#{msg} #{key}"
   end
 
-  def self.long(msg_size, key)
+  def long(msg_size, key)
     (msg_size / key.to_f).ceil
   end
 end
 
 # a = Scytale.encrypt('Hello World', 3, )
-puts Scytale.long(10, 3.0)
+cipher = Scytale.new
+sent = ['Vehicle plan mystery hit solution.',
+        'Adapted valley late practical.',
+        'Fierce remove modern independent under remain.',
+        'Certainly few grew developed farther earliest help.',
+        'Entirely arrange longer especially shine.',
+        'Future smoke harder.',
+        'Symbol star age besides.',
+        'when does telling the truth ever help anybody?']
+
+sent.each do |n|
+  e_msg, msg = cipher.encrypt(n, 3)
+  p msg, e_msg, msg.size, cipher.long(n.size, 3) * 3
+end
