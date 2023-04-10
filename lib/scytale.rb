0ef4ec3dbfa32235101e6ca4d5db515f1cd24a29
3rick <erick.dejesus1@alumno.buap.mx>
2023-04-09 19:24:21 -0600

    add: scytale encrypt and decrypt

--- lib/scytale.rb
+++ lib/scytale.rb
@@ -8,26 +8,38 @@ class Scytale
   end
 
   def decrypt(msg, key)
-    "#{msg} #{key}"
-  end
-
-  def long(msg_size, key)
-    (msg_size / key.to_f).ceil
+    msg.chars.each_slice(key).to_a.transpose.join
   end
 end
 
-# a = Scytale.encrypt('Hello World', 3, )
-cipher = Scytale.new
-sent = ['Vehicle plan mystery hit solution.',
-        'Adapted valley late practical.',
-        'Fierce remove modern independent under remain.',
-        'Certainly few grew developed farther earliest help.',
-        'Entirely arrange longer especially shine.',
-        'Future smoke harder.',
-        'Symbol star age besides.',
-        'when does telling the truth ever help anybody?']
+if $0 == __FILE__
+  cipher = Scytale.new
+  sent = ['Vehicle plan mystery hit solution.',
+          'Adapted valley late practical.',
+          'Fierce remove modern independent under remain.',
+          'Certainly few grew developed farther earliest help.',
+          'Entirely arrange longer especially shine.',
+          'Future smoke harder.',
+          'Symbol star age besides.',
+          'when does telling the truth ever help anybody?',
+          <<~TXT
+            Este es un ejemplo de texto para cifrar con el algoritmo Escítala.
+            Podemos cifrar cualquier texto que queramos, siempre y cuando
+            especifiquemos el número de líneas a utilizar en el cifrado.
+          TXT
+  ]
 
-sent.each do |n|
-  e_msg, msg = cipher.encrypt(n, 3)
-  p msg, e_msg, msg.size, cipher.long(n.size, 3) * 3
+  sent.each do |n|
+    key = rand(2..(n.size / 2))
+    e_msg = cipher.encrypt(n, key)
+    d_msg = cipher.decrypt(e_msg, key)
+    puts <<~TXT
+      it '#{d_msg}' do
+        e_msg = '#{e_msg}'
+        key = #{key}
+        d_msg = '#{d_msg}'
+        expect(cipher.decrypt(e_msg, key)).to eq(d_msg)
+      end
+    TXT
+  end
 end
